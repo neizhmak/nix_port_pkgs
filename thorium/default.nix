e47815124f456c7f8a545cfa5a3d175ddd32d5a7
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-08 21:54:14 +0300

    Update default.nix

--- thorium/default.nix
+++ thorium/default.nix
@@ -1,28 +1,21 @@
-{ pkgs, stdenv, bash }:
+{ appimageTools, fetchurl }:
 let
-  thoriumVersion = "122.0.6261.132";
-  thoriumSrc = {
-    x86_64-linux = "https://github.com/Alex313031/thorium/releases/download/M${thoriumVersion}/Thorium_Browser_${thoriumVersion}_SSE3.AppImage";
-    aarch64-linux = "https://github.com/Alex313031/Thorium-Raspi/releases/download/M${thoriumVersion}/Thorium_Browser_${thoriumVersion}_arm64.AppImage";
+  pname = "thorium";
+  version = "122.0.6261.132";
+
+  src = fetchurl {
+    url = "https://github.com/Alex313031/thorium/releases/download/M${version}/Thorium_Browser_${version}_SSE3.AppImage";
+    sha256 = "sha256-/hMPvYdnVB1XjKgU2v47HnVvW4+uC3rhRjbucqin4iI=";
   };
-  
-  makeThorium = system: let
-    name = "thorium";
-    version = thoriumVersion;
-    src = pkgs.fetchurl {
-      url = thoriumSrc.${system};
-      sha256 = "ijhqMqaCOzPwmGc2GeAONpytJxcoSzyOfVsT0lSBat0=";
-    };
-    appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
-  in pkgs.appimageTools.wrapType2 {
-    inherit name version src;
-    extraInstallCommands = ''
-      install -Dm444 ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
-      install -Dm444 ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
-    '';
+
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
   };
-in
-{
-  thorium_x86_64-linux = makeThorium "x86_64-linux";
-#  thorium_aarch64-linux = makeThorium "aarch64-linux";
+in appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    install -Dm444 ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
+    install -Dm444 ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
+  '';
 }
