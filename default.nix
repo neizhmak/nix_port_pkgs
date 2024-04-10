3daade7a598066167b7fe454e8e9e49d5cc56108
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-10 15:42:22 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,29 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
   
 rec {
+  thorium = pkgs.callPackage ./thorium { };
+  thorium_avx2 = pkgs.callPackage ./thorium_avx3 { };
   thorium_sse3 = pkgs.callPackage ./thorium_sse3 { };
 }
-
-#{ pkgs ? import <nixpkgs> { }
-#, lib ? import <nixpkgs/lib>
-#}:
-#let
-#  pname = "thorium_sse3";
-#  version = "122.0.6261.132";
-#
-#  src = pkgs.fetchurl {
-#    url = "https://github.com/Alex313031/thorium/releases/download/M${version}/Thorium_Browser_${version}_SSE3.AppImage";
-#    sha256 = "sha256-ijhqMqaCOzPwmGc2GeAONpytJxcoSzyOfVsT0lSBat0=";
-#  };
-#
-#  appimageContents = pkgs.appimageTools.extract {
-#    inherit pname version src;
-#  };
-#in pkgs.appimageTools.wrapType2 {
-#  inherit pname version src;
-#
-#  extraInstallCommands = ''
-#    install -Dm444 ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
-#    install -Dm444 ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
-#  '';
-#}
