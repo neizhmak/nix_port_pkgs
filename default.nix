28b5534749064ea41dcc9471b248a3df30ca3974
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-10 18:11:13 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -4,5 +4,5 @@ rec {
   thorium = pkgs.callPackage ./thorium { };
   thorium_avx2 = pkgs.callPackage ./thorium_avx2 { };
   thorium_sse3 = pkgs.callPackage ./thorium_sse3 { };
-  ayugram = pkgs.callPackage ./ayugram { };
+  ayugram = libsForQt5.callPackage ./ayugram { };
 }
