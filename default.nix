7bc41005cfda4914937fc6e01430b97e09ee74ea
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-10 15:46:00 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -2,6 +2,6 @@
   
 rec {
   thorium = pkgs.callPackage ./thorium { };
-  thorium_avx2 = pkgs.callPackage ./thorium_avx3 { };
+  thorium_avx2 = pkgs.callPackage ./thorium_avx2 { };
   thorium_sse3 = pkgs.callPackage ./thorium_sse3 { };
 }
