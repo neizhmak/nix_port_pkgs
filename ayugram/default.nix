36f4344a1338dac355c09b664b1f7502d47a660f
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-11 15:03:37 +0300

    Update default.nix

--- ayugram/default.nix
+++ ayugram/default.nix
@@ -187,13 +187,15 @@ stdenv.mkDerivation rec {
   };
 
   cmakeFlags = [
+    "-DCMAKE_BUILD_TYPE=Release"
     "-Ddisable_autoupdate=ON"
     # We're allowed to used the API ID of the Snap package:
-    "-DTDESKTOP_API_ID=611335"
-    "-DTDESKTOP_API_HASH=d524b414d21f4d37f08684c1df41ac9c"
+    "-DTDESKTOP_API_ID=2040"
+    "-DTDESKTOP_API_HASH=b18441a1ff607e10a989891a5462e627"
     # See: https://github.com/NixOS/nixpkgs/pull/130827#issuecomment-885212649
     "-DDESKTOP_APP_USE_PACKAGED_FONTS=OFF"
     "-DDESKTOP_APP_DISABLE_SCUDO=ON"
+    "-DDESKTOP_APP_DISABLE_AUTOUPDATE=True"
   ];
 
   preBuild = ''
