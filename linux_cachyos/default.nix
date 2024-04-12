80b6b1554606b752c18b54616cfb9716ecdccf23
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-12 13:22:08 +0300

    Update default.nix

--- linux_cachyos/default.nix
+++ linux_cachyos/default.nix
@@ -1,18 +1,12 @@
-{ fetchurl, buildLinux, ... } @ args:
-
-  buildLinux (args // rec {
-    version = "6.8.5";
-    modDirVersion = version;
-
-#    src = fetchurl {
-#      url = "https://github.com/jsakkine-intel/linux-sgx/archive/v23.tar.gz";
-      # After the first build attempt, look for "hash mismatch" and then 2 lines below at the "got:" line.
-      # Use "sha256-....." value here.
-#      hash = "";
-#    };
-    kernelPatches = [];
-
-    extraConfig = import ./cachyos.x86_64-linux.nix;
-
-    extraMeta.branch = "6.8.5";
-  } // (args.argsOverride or {}))
+{ pkgs, ... }:
+let
+  version = "6.8.5";
+  src = pkgs.fetchurl {
+    url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${version}.tar.xz";
+    hash = "sha256:1h0mzx52q9pvdv7rhnvb8g68i7bnlc9rf8gy9qn4alsxq4g28zm8";
+  };
+  configfile = import ./cachyos.x86_64-linux.nix;
+in pkgs.linuxManualConfig {
+    inherit version src configfile;
+    allowImportFromDerivation = true;
+};
