861bf8fc2efe0a4906caa4cabe7eedc1d03c853f
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-12 12:48:18 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -4,5 +4,5 @@ rec {
   thorium = pkgs.callPackage ./thorium { };
   thorium_avx2 = pkgs.callPackage ./thorium_avx2 { };
   thorium_sse3 = pkgs.callPackage ./thorium_sse3 { };
-  linux_cachyos = pkgs.callPackage ./linux_cachyos { };
+  linux_cachyos = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor pkgs.callPackage ./linux_cachyos { };);
 }
