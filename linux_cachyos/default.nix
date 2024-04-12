2099327316fdda59b2e1911570c59937c2fd2a82
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-12 12:56:39 +0300

    Update default.nix

--- linux_cachyos/default.nix
+++ linux_cachyos/default.nix
@@ -1,20 +1,20 @@
 { fetchurl, buildLinux, ... } @ args:
 
   buildLinux (args // rec {
-  version = "5.4.0-rc3";
-  modDirVersion = version;
+    version = "5.4.0-rc3";
+    modDirVersion = version;
 
-  src = fetchurl {
-    url = "https://github.com/jsakkine-intel/linux-sgx/archive/v23.tar.gz";
-    # After the first build attempt, look for "hash mismatch" and then 2 lines below at the "got:" line.
-    # Use "sha256-....." value here.
-    hash = "";
-  };
-  kernelPatches = [];
+    src = fetchurl {
+      url = "https://github.com/jsakkine-intel/linux-sgx/archive/v23.tar.gz";
+      # After the first build attempt, look for "hash mismatch" and then 2 lines below at the "got:" line.
+      # Use "sha256-....." value here.
+      hash = "";
+    };
+    kernelPatches = [];
 
-  extraConfig = ''
-    INTEL_SGX y
-  '';
+    extraConfig = ''
+      INTEL_SGX y
+    '';
 
-  extraMeta.branch = "5.4";
-} // (args.argsOverride or {}));
+    extraMeta.branch = "5.4";
+  } // (args.argsOverride or {}))
