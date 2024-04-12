a81fb5f75f74aac7a4a1cc02d8e37df0a302f23f
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-12 15:01:05 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@ in rec {
   thorium = pkgs.callPackage ./thorium { };
   thorium_avx2 = pkgs.callPackage ./thorium_avx2 { };
   thorium_sse3 = pkgs.callPackage ./thorium_sse3 { };
-  nyx = pkgs.callPackage ./linux_cachyos { };
+  nyx = import (builtins.fetchTarball "https://github.com/neizhmak/nyx-nix/archive/refs/heads/master.tar.gz") {};
+#  nyx = pkgs.callPackage ./linux_cachyos { };
 #  linux_cachyos = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_cachyos_pkgs);
 }
