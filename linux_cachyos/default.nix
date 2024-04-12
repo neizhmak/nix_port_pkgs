856b57a9e52e7a9b6ea9a1706393403818c84575
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2024-04-12 13:50:25 +0300

    Update default.nix

--- linux_cachyos/default.nix
+++ linux_cachyos/default.nix
@@ -1,12 +1,31 @@
-{ pkgs, ... }:
+{ pkgs, fetchFromGitHub }:
 let
-  version = "6.8.5";
-  src = pkgs.fetchurl {
-    url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${version}.tar.xz";
-    hash = "sha256:1h0mzx52q9pvdv7rhnvb8g68i7bnlc9rf8gy9qn4alsxq4g28zm8";
+  src = pkgs.fetchFromGitHub {
+    owner "chaotic-cx"
+    repo "nyx";
+    ver "nyxpkgs-unstable"
+    fetchSubmodules = true;
+    hash ""
   };
-  configfile = import ./cachyos.x86_64-linux.nix;
-in pkgs.linuxManualConfig {
-    inherit version src configfile;
-    allowImportFromDerivation = true;
+
+  nyx = import ./overlays/default.nix;
+in rec {
+  inherit nyx;
+  nyx.linuxPackages_cachyos;
 }
+
+
+
+#{ pkgs, ... }:
+#let
+#  version = "6.8.5";
+#  src = pkgs.fetchurl {
+#    url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${version}.tar.xz";
+#    hash = "sha256:1h0mzx52q9pvdv7rhnvb8g68i7bnlc9rf8gy9qn4alsxq4g28zm8";
+#  };
+#  config = ./cachyos.x86_64-linux.nix;
+#  configfile = import config;
+#in pkgs.linuxManualConfig {
+#    inherit version src configfile;
+#    allowImportFromDerivation = true;
+#}
